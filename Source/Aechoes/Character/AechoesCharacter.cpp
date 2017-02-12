423fb599c077ee4951f381aa8043fe318c97705c
Skyler Manzanares <skymanzanares@hotmail.com>
2017-02-12 15:51:16 -0700

    Removed AechoesChar old default stuff

--- Source/Aechoes/Character/AechoesCharacter.cpp
+++ Source/Aechoes/Character/AechoesCharacter.cpp
@@ -11,11 +11,7 @@ AAechoesCharacter::AAechoesCharacter(const class FObjectInitializer& ObjectIniti
 {
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
-
-	// set our turn rates for input
-	BaseTurnRate = 45.f;
-	BaseLookUpRate = 45.f;
-
+	
 	// Don't rotate when the controller rotates. Let that just affect the camera.
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
@@ -28,114 +24,6 @@ AAechoesCharacter::AAechoesCharacter(const class FObjectInitializer& ObjectIniti
 	GetCharacterMovement()->AirControl = 0.2f;
 
 }
-
-//////////////////////////////////////////////////////////////////////////
-// Input
-
-void AAechoesCharacter::SetupPlayerInputComponent(class UInputComponent* input)
-{
-	// Set up gameplay key bindings
-	check(input);
-	//InputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-	//InputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
-
-	//InputComponent->BindAxis("MoveForward", this, &AAechoesCharacter::MoveForward);
-	//InputComponent->BindAxis("MoveRight", this, &AAechoesCharacter::MoveRight);
-
-	//// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	//// "turn" handles devices that provide an absolute delta, such as a mouse.
-	//// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	//InputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	//InputComponent->BindAxis("TurnRate", this, &AAechoesCharacter::TurnAtRate);
-	//InputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	//InputComponent->BindAxis("LookUpRate", this, &AAechoesCharacter::LookUpAtRate);
-
-	// handle touch devices
-    input->BindTouch(IE_Pressed, this, &AAechoesCharacter::TouchStarted);
-    input->BindTouch(IE_Released, this, &AAechoesCharacter::TouchStopped);
-
-    //input->BindAction()
-}
-
-
-void AAechoesCharacter::MoveRightRate(float Rate)
-{
-}
-
-void AAechoesCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
-{
-	// jump, but only on the first touch
-	if (FingerIndex == ETouchIndex::Touch1)
-	{
-		Jump();
-	}
-}
-
-void AAechoesCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
-{
-	if (FingerIndex == ETouchIndex::Touch1)
-	{
-		StopJumping();
-	}
-}
-
-void AAechoesCharacter::OnSelectClick(FVector location)
-{
-}
-
-void AAechoesCharacter::OnActionClick(FVector location)
-{
-}
-
-//void AAechoesCharacter::TurnAtRate(float Rate)
-//{
-//	// calculate delta for this frame from the rate information
-//	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
-//}
-//
-//void AAechoesCharacter::LookUpAtRate(float Rate)
-//{
-//	// calculate delta for this frame from the rate information
-//	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
-//}
-//
-//void AAechoesCharacter::MoveForward(float Value)
-//{
-//	if ((Controller != NULL) && (Value != 0.0f))
-//	{
-//		// find out which way is forward
-//		const FRotator Rotation = Controller->GetControlRotation();
-//		const FRotator YawRotation(0, Rotation.Yaw, 0);
-//
-//		// get forward vector
-//		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-//		AddMovementInput(Direction, Value);
-//	}
-//}
-//
-//void AAechoesCharacter::MoveUp(float Value)
-//{
-//}
-//
-//void AAechoesCharacter::MoveRight(float Value)
-//{
-//	if ( (Controller != NULL) && (Value != 0.0f) )
-//	{
-//		// find out which way is right
-//		const FRotator Rotation = Controller->GetControlRotation();
-//		const FRotator YawRotation(0, Rotation.Yaw, 0);
-//	
-//		// get right vector 
-//		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-//		// add movement in that direction
-//		AddMovementInput(Direction, Value);
-//	}
-//}
-//
-//void AAechoesCharacter::MoveUpRate(float Rate)
-//{
-//}
-
 
 void AAechoesCharacter::BeginPlay() {
 	Super::BeginPlay();
--- Source/Aechoes/Character/AechoesCharacter.h
+++ Source/Aechoes/Character/AechoesCharacter.h
@@ -10,68 +10,8 @@ class AAechoesCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
-
-	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
-
 public:
 	AAechoesCharacter(const class FObjectInitializer& ObjectInitializer);
-
-	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseTurnRate;
-
-	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseLookUpRate;
-
-protected:
-
-	/** Called for up/down input */
-	void MoveUp(float Value);
-
-	/** Called for side to side input */
-	void MoveRight(float Value);
-
-	/** 
-	 * Called via input to go up or down at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void MoveUpRate(float Rate);
-
-	/**
-	 * Called via input to turn look side-to-side at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void MoveRightRate(float Rate);
-
-	/** Handler for when a touch input begins. */
-	void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
-
-	/** Handler for when a touch input stops. */
-	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
-
-    /** Handles left clicks **/
-    void OnSelectClick(FVector location);
-
-    /** Handles right clicks **/
-    void OnActionClick(FVector location);
-
-protected:
-	// APawn interface
-	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
-	// End of APawn interface
-
-public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
-
 
 	virtual void BeginPlay() override;
 };
